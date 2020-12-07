5944a1024de5fcb046d4188635a756fb10273a6e
Amela <amela_korjenic@hotmail.com>
2020-12-07 11:42:39 +0100

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <p>Copyright {new Date().getFullYear()}. All rights reserved</p>
+    </div>
+  );
+}
+
+export default Footer;
