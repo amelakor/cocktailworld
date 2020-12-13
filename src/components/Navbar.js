3cd62203d257fac6c4dc40fd13d607b4ba952915
Amela <amela_korjenic@hotmail.com>
2020-12-13 12:13:18 +0100

    UI changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../logo.svg";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
-        <Link to="/">
-          <img src={logo} alt="cocktail" className="logo" />
+        <Link to="/" className="logo">
+          Cocktails
         </Link>
         <ul className="nav-links">
           <li>
